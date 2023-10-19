3f372052b0d3ccd5fc9671a700ce0dde3da786a5
pgcokazaki <okazaki@propagandist.co.jp>
2023-10-19 23:25:07 +0900

    20231020リリース ブランチBの修正

--- src/com/tutorial/git/sample1.java
+++ src/com/tutorial/git/sample1.java
@@ -5,7 +5,7 @@ public class sample1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("機能追加");
-		System.out.println("機能追加ブランチ２");
+		System.out.println("ブランチBの修正");
 	}
 
 }
